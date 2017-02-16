a9131a88821908095905891da7d7d6c88dee4bc6
Norm Evangelista <norm.evangelista@gmail.com>
2017-02-15 22:39:57 -0800

    Conformed implementation more to Algorithm Design Manual's

--- graphis/Graphis.hpp
+++ graphis/Graphis.hpp
@@ -30,11 +30,22 @@ struct AdjacencyNode
     int weight;
 };
 
+//! \enum   VisitedState
+enum VisitedState
+{
+    e_undiscovered,
+    e_discovered,
+    e_processed
+};
+
 template<typename DataT>
 using AdjacencyList = std::list<AdjacencyNode<DataT>>;
 
 template<typename DataT>
 using EdgeList = std::map<DataT, AdjacencyList<DataT>>;
+
+template<typename DataT>
+using DegreeList = std::map<DataT, int>;
 
 //! class   Graphis
 template<typename DataT>
@@ -44,12 +55,14 @@ public:
     //! \fn     Graphis::Graphis
     Graphis(bool is_directed = false)
     : m_num_vertices(0)
+    , m_num_edges(0)
     , m_is_directed(is_directed)
     {}
 
     //! \fn     Graphis::Graphis
     Graphis(int num_vertices, bool is_directed)
     : m_num_vertices(num_vertices)
+    , m_num_edges(0)
     , m_is_directed(is_directed)
     {}
 
@@ -59,6 +72,7 @@ public:
         // Add edge from src to dst
         DoAddEdge(src, dst);
 
+        // Add edge from dst to src for undirected graph
         if (!m_is_directed) {
             DoAddEdge(dst, src);
         }
@@ -94,6 +108,12 @@ public:
         }
     }
 
+    //! \fn     GetNumEdges
+    int GetNumEdges() const
+    {
+        return m_num_edges;
+    }
+
     //! \fn     GetNumVerts
     int GetNumVerts() const
     {
@@ -113,23 +133,35 @@ public:
 
 private:
     //! \fn     DoAddEdge
+    //! \brief  Adds an edge from src to dst; src is the key, all edges from it reside in its edge list
     void DoAddEdge(DataT src, DataT dst)
     {
         // Add edge from src to dst
         AdjacencyNode<DataT> dstnode(dst);
-        auto srcit = m_edges.find(src);
-        if (srcit == m_edges.end()) {
+
+        auto anedge = m_edges.find(src);
+        if (anedge == m_edges.end()) {
             ++m_num_vertices;
             AdjacencyList<DataT> nulist;
             m_edges.insert(std::make_pair(src, nulist));
         }
 
+        auto adegree = m_degrees.find(src);
+        if (adegree == m_degrees.end()) {
+            int num_degrees = 0;
+            m_degrees.insert(std::make_pair(src, num_degrees));
+        }
+
+        ++m_degrees[src];
         m_edges[src].push_front(dstnode);
+        ++m_num_edges;
     }
 
     int m_num_vertices;
+    int m_num_edges;
     bool m_is_directed;
     EdgeList<DataT> m_edges;
+    DegreeList<DataT> m_degrees;
 };
 
 #endif //GRAPHIS_GRAPHIS_HPP
