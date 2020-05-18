4fcadb91b568d56823ceb06da0417cf0536ced95
Norm Evangelista <norm.evangelista@gmail.com>
2020-05-18 11:44:48 -0700

    Updated to C++17, conformed to current coding standards
    Signed-off-by: Norm Evangelista <norm.evangelista@gmail.com>

--- circula/Circula.h
+++ circula/Circula.h
@@ -1,95 +1,77 @@
 /*! -------------------------------------------------------------------------*\
 |   Circular buffer after https://embedjournal.com/implementing-circular-buffer-embedded-c/
 \*---------------------------------------------------------------------------*/
-#ifndef CIRCULA_CIRCULA
-#define CIRCULA_CIRCULA
+#pragma once
 
 #include <array>
 #include <stdexcept>
 #include <vector>
 
-//! \class  Circula
-//! \brief  Circular buffer, size specified at compile time
-//! \note   Buffer of size N contains N+1 slots
-template <typename DataT, std::size_t N>
-class Circula
-{
+/// \class  Circula
+/// \brief  Circular buffer, size specified at compile time
+/// \note   Buffer of size N contains N+1 slots
+template<typename DataT, std::size_t N>
+class Circula {
 public:
-    //!
-    Circula()
-        : m_head(0)
-        , m_tail(0)
-        , m_capacity(N+1)
-    {
-    }
+    ///
+    Circula() : _capacity(N + 1) {}
 
-    //!
+    ///
     virtual ~Circula() = default;
 
-    //! \fn     IsEmpty
-    bool IsEmpty() const
-    {
-        return (m_head == m_tail);
+    ///
+    [[nodiscard]] bool isEmpty() const {
+        return (_head == _tail);
     }
 
-    std::size_t Capacity() const
-    {
+    [[nodiscard]] std::size_t capacity() const {
         return N;
     }
 
-    //! \fn     Pop
-    //! \brief  Values are popped from the buffer at the tail
-    DataT Pop()
-    {
-        if (IsEmpty())
-        {
+    ///
+    /// \brief  Values are popped from the buffer at the tail
+    DataT pop() {
+        if (isEmpty()) {
             throw std::underflow_error("Buffer empty");
         }
 
-        int next = m_tail + 1;
+        int next{_tail + 1};
 
         // Wrap the tail
-        if (next >= m_capacity)
-        {
+        if (next >= _capacity) {
             next = 0;
         }
 
-        // Pop from the tail, and advance the tail
-        DataT data = m_buffer[m_tail];
-        m_tail = next;
+        // pop from the tail, and advance the tail
+        DataT data{_buffer[_tail]};
+        _tail = next;
 
         return data;
     }
 
-    //! \fn     Push
-    //! \brief  Values are pushed onto the buffer at the head
-    void Push(DataT data)
-    {
-        int next = m_head + 1;
+    ///
+    /// \brief  Values are pushed onto the buffer at the head
+    void push(DataT data) {
+        int next{_head + 1};
 
         // Wrap the head
-        if (next >= m_capacity)
-        {
+        if (next >= _capacity) {
             next = 0;
         }
 
         // Definition: If head + 1 == tail, the buffer is full
-        if (next == m_tail)
-        {
+        if (next == _tail) {
             throw std::overflow_error("Buffer full");
         }
 
-        // Push at the head, and advance the head
-        m_buffer[m_head] = data;
-        m_head = next;
+        // push at the head, and advance the head
+        _buffer[_head] = data;
+        _head = next;
     }
 
 private:
-    int m_head;
-    int m_tail;
-    DataT m_buffer[N+1];
-    std::size_t m_capacity;
-
+    int _head{0};
+    int _tail{0};
+    DataT _buffer[N + 1];
+    std::size_t _capacity;
 };
-
-#endif // CIRCULA_CIRCULA
